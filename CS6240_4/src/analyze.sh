2ae257b569c430788ef49569cbc21c18a42be3f9
Adib Alwani <alwani.a@husky.neu.edu>
2016-02-11 17:11:45 -0500

    Added slope

--- CS6240_4/src/analyze.sh
+++ CS6240_4/src/analyze.sh
@@ -9,15 +9,19 @@ echo -n "" > summary-flight-time
 
 for f in $FILES_TIME
 do
-	cat $f | grep "Residual standard error" | awk '{print substr("'"$f"'", 9, 2) " " $4}' >> summary-flight-time
+	cat $f | grep "Residual standard error" | awk '{printf substr("'"$f"'", 9, 2) " " $4}' >> summary-flight-time
+	cat $f | grep "^df$flight_time" | awk '{print " " $2}' >> summary-flight-time
 done
 
 for f in $FILES_DISTANCE
 do
-	cat $f | grep "Residual standard error" | awk '{print substr("'"$f"'", 9, 2) " " $4}' >> summary-distance
+	cat $f | grep "Residual standard error" | awk '{printf substr("'"$f"'", 9, 2) " " $4}' >> summary-distance
+	cat $f | grep "^df$distance_traveled" | awk '{print " " $2}' >> summary-distance
 done
 
-echo "carrier_code flight_time distance" > analysis
+
+
+echo "carrier_code flight_time slope_flight_time distance slope_distance" > analysis
 join summary-flight-time summary-distance >> analysis
 
-rm -f summary-distance summary-flight-time
+#rm -f summary-distance summary-flight-time
