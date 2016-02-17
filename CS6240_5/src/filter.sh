7c8df3a7540ee597a216033bee3d26c4292f7ebb
Adib Alwani <alwani.a@husky.neu.edu>
2016-02-17 17:43:12 -0500

    script

--- CS6240_5/src/filter.sh
+++ CS6240_5/src/filter.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
-# Author: Rachit Puri
+# Author: Adib Alwani
 
-awk '{print $1 " " $2 " " $4 " " $5}' test | sort -t, -nk1 > solution
+MISSED_SUM=0
+CONNECTING_SUM=0
+KEY=""
+
+echo -n "" > solution_final
+
+while read line
+do
+	KEY_LINE="$(echo $line | awk '{print $1 " " $2}')"
+	MISSED_SUM_LINE="$(echo $line | awk '{print $4}')"
+	CONNECTING_SUM_LINE="$(echo $line | awk '{print $5}')"
+	if [ "$KEY" = "$KEY_LINE" ]
+	then
+		MISSED_SUM=$((MISSED_SUM+MISSED_SUM_LINE))
+		CONNECTING_SUM=$((CONNECTING_SUM+CONNECTING_SUM_LINE))
+	else
+		echo $KEY " " $MISSED_SUM " " $CONNECTING_SUM >> solution_final
+		KEY=$KEY_LINE
+		MISSED_SUM=0
+		CONNECTING_SUM=0
+	fi
+done < final_backup
+
+sort solution_final | sed -i '1d' solution_final
