00da0b6a196a61bebb02953118283c2124f77eb5
Adib Alwani <alwani.a@husky.neu.edu>
2016-03-17 21:54:05 -0400

    Routing

--- CS6240_8/src/Routing.java
+++ CS6240_8/src/Routing.java
@@ -8,23 +8,35 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+/**
+ * MapReduce program to to generate predicted optimal two-flight routes
+ * 
+ * @author Adib Alwani
+ */
 public class Routing extends Configured implements Tool {
-	
 
-	private boolean buildModel(String[] args) {
+	/**
+	 * Run MapReduce job to build model
+	 * 
+	 * @param args Command line args
+	 * @return Job report
+	 * @throws Exception
+	 */
+	private boolean buildModel(String[] args) throws Exception {
 		Job job = Job.getInstance(getConf());
 		job.setJar("job.jar");
-		job.setMapperClass(RoutingMapper.class);
-		job.setReducerClass(RoutingReducer.class);
+		job.setMapperClass(ModelMapper.class);
+		job.setReducerClass(ModelReducer.class);
 		job.setMapOutputKeyClass(Text.class);
-		job.setMapOutputValueClass(RoutingFlight.class);
+		job.setMapOutputValueClass(FlightDetail.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(BytesWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		return job.waitForCompletion(true);
 	}
 	
-	// request file, input file
+	/*// request file, input file
 	private boolean PossibleConnections(String[] args) {
 		Job job = Job.getInstance(getConf());
 		job.setJar("job.jar");
@@ -36,11 +48,11 @@ public class Routing extends Configured implements Tool {
 		job.setOutputValueClass(BytesWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-	}
+	}*/
 	
 	@Override
 	public int run(String[] args) throws Exception {
-		return buildModel(args) && PossibleConnections(args) ? 0 : 1;
+		return buildModel(args) ? 0 : 1;
 	}
 
 	public static void main(String[] args) {
