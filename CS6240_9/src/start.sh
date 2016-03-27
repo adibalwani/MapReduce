2f41166d0b9da341dde39ce83e05cf4f394ab57e
rushibadami1992 <rushibadami1992@gmail.com>
2016-03-27 17:35:43 -0400

    Ec2 instance start

---
+++ CS6240_9/src/start.sh
@@ -0,0 +1,9 @@
+ec2-start-instances instance_id [instance_id...]
+sleep 50
+ec2-start-instances instance_id [instance_id...]
+sleep 50
+ec2-start-instances instance_id [instance_id...]
+sleep 50
+ec2-start-instances instance_id [instance_id...]
+sleep 50
+
