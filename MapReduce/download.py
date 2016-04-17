c9cbf1515b2db3ef6940ecd86301645ae3a67317
rushibadami1992 <rushibadami1992@gmail.com>
2016-04-17 19:43:57 -0400

    Download files for working nodes

---
+++ MapReduce/download.py
@@ -0,0 +1,17 @@
+# Author: Rachit Puri and Rushikesh Badami
+
+import sys
+import os
+from subprocess import call
+import re
+
+for f in os.listdir('.'):
+	if re.match('s3data', f):
+		filename = f
+		print filename
+
+with open(filename, 'r') as fd:
+	for line in fd:
+		call(["aws", "s3", "cp", line[:-1], "./input"])
+
+fd.close()
