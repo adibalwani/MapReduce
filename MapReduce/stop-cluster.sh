6157094a66758d854e67484be114cb8d50f4c370
rushibadami1992 <rushibadami1992@gmail.com>
2016-04-24 19:47:22 -0400

    Added comments to ths file

--- MapReduce/stop-cluster.sh
+++ MapReduce/stop-cluster.sh
@@ -4,5 +4,5 @@
 # Terminate Instances
 while read id
 do
-	aws ec2 terminate-instances --instance-ids $id
+	aws ec2 terminate-instances --instance-ids $id  # Command used to terminate all ec2 instances
 done < instance-ids
